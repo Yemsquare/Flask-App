54009cbb300b1c893be0781bea494a2b87c5c9a6
Yemsquare <ibiyemioluyemi@gmail.com>
2023-02-03 10:22:39 +0100

    Note module added

--- website/auth.py
+++ website/auth.py
@@ -24,7 +24,7 @@ def login():
             flash('Email does not exit.', category='error')
 
 
-    return render_template("login.html", boolean=True)
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -60,4 +60,4 @@ def sign_up():
             return redirect(url_for('views.home'))
 
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
